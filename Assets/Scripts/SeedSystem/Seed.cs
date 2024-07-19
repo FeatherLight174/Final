9a0c8c0a19800ddea214e5abeb7e059f1fe0cf07
青空 <113504120+qingkongg@users.noreply.github.com>
2024-07-19 20:39:56 +0800

    Seed system compelete

--- Assets/Scripts/SeedSystem/Seed.cs
+++ Assets/Scripts/SeedSystem/Seed.cs
@@ -5,7 +5,7 @@ using MouseState;
 
 public class Seed : MonoBehaviour
 {
-    private bool m_IsActive = false;
+    private bool m_IsActive = true;
     public GameObject Gold;
     public GameObject Tower;
     public GameObject Power;
@@ -28,30 +28,45 @@ public class Seed : MonoBehaviour
     {
         // 当鼠标点击触发器时执行此方法
         Debug.Log("Clicked on object: ");
-        if (MouseController.NowMouse == mouseState.Gold)
+        if (m_IsActive)
         {
-            if (GoldAndElectricity.gold >= PriceGold)
+            if (MouseController.NowMouse == mouseState.Gold)
             {
-                Instantiate(Gold, transform.position, Quaternion.identity);
+                if (GoldAndElectricity.gold >= PriceGold)
+                {
+                    Instantiate(Gold, transform.position, Quaternion.identity);
+                    m_IsActive = false;
+                }
+                MouseController.NowMouse = mouseState.None;
             }
-            MouseController.NowMouse = mouseState.None;
+            else if (MouseController.NowMouse == mouseState.Tower)
+            {
+                if (GoldAndElectricity.gold >= PriceTower)
+                {
+                    Instantiate(Tower, transform.position, Quaternion.identity);
+                    m_IsActive = false;
+                }
+                MouseController.NowMouse = mouseState.None;
+            }
+            else if (MouseController.NowMouse == mouseState.Power)
+            {
+                if (GoldAndElectricity.gold >= PricePower)
+                {
+                    Instantiate(Power, transform.position, Quaternion.identity);
+                    m_IsActive = false;
+                }
+                MouseController.NowMouse = mouseState.None;
+            }
+            else if (MouseController.NowMouse == mouseState.Shield)
+            {
+                if (GoldAndElectricity.gold <= PriceShield)
+                {
+                    Instantiate(Shield, transform.position, Quaternion.identity);
+                    m_IsActive = false;
+                }
+                MouseController.NowMouse = mouseState.None;
+            }
         }
-        else if(MouseController.NowMouse == mouseState.Tower)
-        {
-            Instantiate(Tower, transform.position, Quaternion.identity);
-            MouseController.NowMouse = mouseState.None;
-        }
-        else if(MouseController.NowMouse == mouseState.Power)
-        {
-            Instantiate(Power, transform.position, Quaternion.identity);
-            MouseController.NowMouse = mouseState.None;
-        }
-        else if(MouseController.NowMouse == mouseState.Shield)
-        {
-            Instantiate(Gold, transform.position, Quaternion.identity);
-            MouseController.NowMouse = mouseState.None;
-        }
-
 
     }    // 调用其他方法处理点击事件，例如改变物体颜色
         
