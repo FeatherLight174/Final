cf452d1b4d13222768b0b7c670a7ba6b632e93c9
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2024-07-20 14:06:21 +0800

    111

--- Assets/Scripts/EconomySystem/Base/Base.cs
+++ Assets/Scripts/EconomySystem/Base/Base.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Base : MonoBehaviour
 {
+    //
+    public GameObject upgradePanel;
+
+
     //HP
     public float[] fullHP;
     private float currentHP;
@@ -23,6 +27,9 @@ public class Base : MonoBehaviour
     // Upgrade
     public float upgrade_2;
     public float upgrade_3;
+
+    // Panel flag
+    private int flag = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +55,21 @@ public class Base : MonoBehaviour
             GoldAndElectricity.electricity += ElectricityPerTime[level - 1];
             m_TimerElectricity = 0;
         }
+    }
+
+    private void OnMouseDown()
+    {
+        if (flag % 2 == 0)
+        {
+            upgradePanel.SetActive(true);
+            //feature.SetActive(true);
+        }
+        else if (flag % 2 == 1)
+        {
+            upgradePanel.SetActive(false);
+            //feature.SetActive(false);
+        }
+        flag++;
     }
 
     public void Upgrade()
