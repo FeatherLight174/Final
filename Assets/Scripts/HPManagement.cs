1458afcedac627967cbe751cd194f69d1022d565
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2024-07-20 17:06:06 +0800

    map

--- Assets/Scripts/HPManagement.cs
+++ Assets/Scripts/HPManagement.cs
@@ -8,7 +8,7 @@ public class HPManagement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        HP = gameObject.
+        
     }
 
     // Update is called once per frame
