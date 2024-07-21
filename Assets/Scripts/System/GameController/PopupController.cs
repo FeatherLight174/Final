865953116939de7a78f340d3ccb3eb41adb7675f
青空 <113504120+qingkongg@users.noreply.github.com>
2024-07-21 15:43:07 +0800

    info show

--- Assets/Scripts/System/GameController/PopupController.cs
+++ Assets/Scripts/System/GameController/PopupController.cs
@@ -1,10 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PopupController : MonoBehaviour
 {
-    public GameObject popupObject;
+    public GameObject Sell;
+    private Collider2D popupCollider;
+    public GameObject Upgrade;
+    public GameObject Range;
+
+    void Start()
+    {
+        // 缓存弹窗物体的 Collider2D 组件
+        if (Sell != null)
+        {
+            popupCollider = Sell.GetComponent<Collider2D>();
+            Sell.SetActive(false); // 开始时隐藏弹窗
+        }
+        Upgrade.SetActive(false);
+        Range.SetActive(false);
+    }
 
     void Update()
     {
@@ -15,13 +28,32 @@ public class PopupController : MonoBehaviour
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = 0; // 如果是2D弹窗，确保Z轴位置为0
 
-            // 如果点击在弹窗物体上，直接返回
-            Collider2D popupCollider = popupObject.GetComponent<Collider2D>();
-            if (popupCollider != null && popupCollider.OverlapPoint(mousePosition))
+            // 如果弹窗已激活且点击在弹窗物体上，直接返回
+            if (Sell.activeSelf && popupCollider != null && gameObject.GetComponent<Collider2D>().OverlapPoint(mousePosition) && Upgrade.activeSelf &&(Range == null || Range.activeSelf))
+            {
                 return;
+            }
+            if(Range != null && Range.activeSelf)
+            {
+                Range.SetActive(false) ;
+            }
+            // 如果弹窗已激活且点击在弹窗物体外部，隐藏弹窗
+            if (Sell.activeSelf || Upgrade.activeSelf)
+            {
+                Sell.SetActive(false);
+                Upgrade.SetActive(false);
+                return;
+            }
 
-           
-            popupObject.SetActive(false);
+            // 尝试点击其他物体
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+            if (hit.collider != null)
+            {
+                // 在此处处理点击物体展示信息的逻辑
+                Sell.SetActive(true);
+                Upgrade.SetActive(true);
+                Range.SetActive(true);
+            }
         }
     }
 }
