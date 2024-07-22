7909e3368500fc1f80cec304f0cd1d14cc40302b
chlorofluorocarbon <clarence_d@126.com>
2024-07-22 18:39:50 +0800

    Update TowerUpgrade.cs

--- Assets/Scripts/Tower/TowerUpgrade.cs
+++ Assets/Scripts/Tower/TowerUpgrade.cs
@@ -7,15 +7,15 @@ public class TowerUpgrade : MonoBehaviour
     public TowerController tower;
     private void Update()
     {
-        if (tower.towerLevel >= 3)
+        if (tower.towerLevel >= Base.level)
         {
-            tower.towerLevel = 3;
+            tower.towerLevel = Base.level;
             GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
         }
     }
     private void OnMouseDown()
     {
-        if (tower.towerLevel <= 2)
+        if (tower.towerLevel < Base.level)
         {
             tower.Upgrade();
         }
