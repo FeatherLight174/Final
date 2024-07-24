3daacab8d230833f4bcbbdf3e9f3f12d89cce153
chlorofluorocarbon <clarence_d@126.com>
2024-07-24 21:18:22 +0800

    Update EnemyBeingAttacked.cs

--- Assets/Scripts/Enemy/HealthBar/EnemyBeingAttacked.cs
+++ Assets/Scripts/Enemy/HealthBar/EnemyBeingAttacked.cs
@@ -6,15 +6,22 @@ public class EnemyBeingAttacked : MonoBehaviour
 {
     public float IsAttacked = 0;
     public float showTime;
+    private HPManagement health;
+    private float HP;
     void Start()
     {
-        
+        health = GetComponentInParent<HPManagement>();
     }
 
     // Update is called once per frame
     void Update()
     {
         IsAttacked -= Time.deltaTime;
+        if (health.HP != HP)
+        {
+            HP = health.HP;
+            IsAttacked = showTime;
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
