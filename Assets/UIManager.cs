c18349b54101fe564f55ca3a0ae599825c8e2ab1
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2024-07-25 22:00:48 +0800

    Update UIManager.cs

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -25,13 +25,13 @@ public class UIManager : MonoBehaviour
     }
 
     // 显示塔的信息
-    public void ShowTowerInfo(Tower tower)
+    /*public void ShowTowerInfo(Tower tower)
     {
         infoPanel.SetActive(true);
         nameText.text = "Name: " + tower.towerName;
         damageText.text = "Damage: " + tower.damage.ToString();
         rangeText.text = "Range: " + tower.range.ToString();
         fireRateText.text = "Fire Rate: " + tower.fireRate.ToString();
-    }
+    }*/
 }
 
