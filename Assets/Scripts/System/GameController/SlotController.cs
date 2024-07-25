c6702286c56fc821998cf0a47fc81c60457687bc
FeatherLight174 <songyh2022@shanghaitech.edu.cn>
2024-07-25 21:48:59 +0800

    11

---
+++ Assets/Scripts/System/GameController/SlotController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MouseState;
+
+public class SlotController : MonoBehaviour
+{
+    public GameObject gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MouseController.NowMouse != mouseState.None){
+            gm.SetActive(true);
+        }
+        else
+        {
+            gm.SetActive(false);
+        }
+    }
+
+
+}
